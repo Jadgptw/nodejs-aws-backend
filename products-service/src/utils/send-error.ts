b642dbdfa95aeed18bd0615af9698d7a789668ce
Aliaksandr Hordzin <Aliaksandr_Hordzin@epam.com>
2020-11-03 09:05:41 +0300

    Task-3 - fix swagger file and remove error stack trace from the response

--- products-service/src/utils/send-error.ts
+++ products-service/src/utils/send-error.ts
@@ -1,12 +1,12 @@
-import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
+import { INTERNAL_SERVER_ERROR, getStatusText } from 'http-status-codes';
 
 import { AppError } from "../models/app-error";
 
 export const sendError = (error: AppError) => {
-  const { statusCode = INTERNAL_SERVER_ERROR } = error;
+  const { message, statusCode = INTERNAL_SERVER_ERROR, status = getStatusText(INTERNAL_SERVER_ERROR) } = error;
 
   return {
     statusCode,
-    body: JSON.stringify({ error })
+    body: JSON.stringify({ error: { message, status } })
   }
 };
